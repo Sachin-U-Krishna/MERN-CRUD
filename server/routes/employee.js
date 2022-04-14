69c6c989cd2b756c174871c87ea4acc4fae3f3e2
Sachin U Krishna <89995107+Sachin-U-Krishna@users.noreply.github.com>
2022-04-14 23:21:46 +0530

    Add files via upload

--- server/routes/employee.js
+++ server/routes/employee.js
@@ -26,5 +26,49 @@ empRouter.route('/')
         })
         .catch((err) => next(err))
     })
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /employee');
+    })
+    .delete((req, res, next) => {
+        res.statusCode = 403;
+        res.end('Forbidden action on /employee');
+    })
+
+empRouter.route('/:id')
+    .get((req,res,next) => {
+        Employees.findById(req.params.id)
+        .then((emp) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(emp);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+    })
+    .post((req, res, next) => {
+        res.statusCode = 403;
+        res.end('POST operation not supported on /employee/'+ req.params.id);
+    })
+    .put((req, res, next) => {
+        Employees.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, { new: true })
+        .then((emp) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(emp);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+    })
+    .delete((req, res, next) => {
+        Employees.findByIdAndRemove(req.params.id)
+        .then((resp) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(resp);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+    });
+
 
 module.exports = empRouter;
